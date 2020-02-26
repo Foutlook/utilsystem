5d4c20129ef5b090dc3f370e9941933b42911a03
xingkai.fan <xingkai.fan@wwwarehouse.com>
2020-02-26 13:17:06 +0800

    图片水印

--- src/main/java/com/foutin/utils/ImageOperationUtils.java
+++ src/main/java/com/foutin/utils/ImageOperationUtils.java
@@ -8,7 +8,10 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 
 /**
  * @author xingkai.fan
@@ -17,13 +20,14 @@ import java.io.IOException;
  */
 public class ImageOperationUtils {
 
-    private static final float DEFAULT_OPACITY = 0.25f;
+    private static final float DEFAULT_OPACITY = 1f;
 
     /**
      * 添加图片型水印
+     *
      * @param resourceImageUrl 需加水印的图片
-     * @param targetImageUrl 保存图片路径
-     * @param waterMarkUrl 水印图片
+     * @param targetImageUrl   保存图片路径
+     * @param waterMarkUrl     水印图片
      * @throws Exception
      */
     public static void setImageWatermark(String resourceImageUrl, String targetImageUrl, String waterMarkUrl) throws Exception {
@@ -46,13 +50,13 @@ public class ImageOperationUtils {
 
     /**
      * 给图片添加文字型水印
+     *
      * @param resourceImageUrl 须加水印图片
-     * @param targetImageUrl 保存图片路径和名
-     * @param text 水印文字
-     * @param fontSize 字体大小
+     * @param targetImageUrl   保存图片路径和名
+     * @param text             水印文字
      * @throws IOException
      */
-    public static void setTextWatermark(String resourceImageUrl, String targetImageUrl, String text, int fontSize) throws IOException {
+    public static void setTextWatermark(String resourceImageUrl, String targetImageUrl, String text) throws IOException {
 
         // 获取原图文件
         File resourceFile = new File(resourceImageUrl);
@@ -60,7 +64,7 @@ public class ImageOperationUtils {
         // ImageIO读取图片
         BufferedImage image = ImageIO.read(resourceFile);
 
-        BufferedImage bufferedImage = handleTextWaterMark(text, image.getWidth(), image.getHeight(), fontSize);
+        BufferedImage bufferedImage = handleTextWaterMark(text, image.getWidth(), image.getHeight());
 
         Thumbnails.of(image)
                 // 设置图片大小
@@ -72,15 +76,78 @@ public class ImageOperationUtils {
     }
 
     /**
+     * 给图片添加文字型水印
+     *
+     * @param resourceImageUrl 须加水印图片
+     * @param text             水印文字
+     * @return BufferedImage
+     * @throws IOException
+     */
+    public static BufferedImage setTextWatermark(String resourceImageUrl, String text) throws IOException {
+
+        // 获取原图文件
+        File resourceFile = new File(resourceImageUrl);
+        // ImageIO读取图片
+        BufferedImage image = ImageIO.read(resourceFile);
+
+        BufferedImage bufferedImage = handleTextWaterMark(text, image.getWidth(), image.getHeight());
+
+        return Thumbnails.of(image)
+                // 设置图片大小
+                .size(image.getWidth(), image.getHeight())
+                // 加水印 参数：1.水印位置 2.水印图片 3.不透明度0.0-1.0
+                .watermark(Positions.BOTTOM_RIGHT, bufferedImage, DEFAULT_OPACITY)
+                // 输出到文件
+                .asBufferedImage();
+    }
+
+    /**
+     * 给图片添加文字型水印
+     *
+     * @param resourceImageUrl 须加水印图片
+     * @param outputStream 输出流
+     * @param text             水印文字
+     * @return BufferedImage
+     * @throws IOException
+     */
+    public static void setTextWatermark(String resourceImageUrl, OutputStream outputStream, String text) throws IOException {
+
+        // 获取原图文件
+        File resourceFile = new File(resourceImageUrl);
+        // ImageIO读取图片
+        BufferedImage image = ImageIO.read(resourceFile);
+
+        BufferedImage bufferedImage = handleTextWaterMark(text, image.getWidth(), image.getHeight());
+
+        Thumbnails.of(image)
+                // 设置图片大小
+                .size(image.getWidth(), image.getHeight())
+                // 加水印 参数：1.水印位置 2.水印图片 3.不透明度0.0-1.0
+                .watermark(Positions.BOTTOM_RIGHT, bufferedImage, DEFAULT_OPACITY)
+                .outputFormat("jpg")
+                // 输出到文件
+                .toOutputStream(outputStream);
+
+    }
+
+    /**
      * 操纵文字型水印
-     * @param text 水印文本
-     * @param width 需要添加水印的图片宽度
+     *
+     * @param text   水印文本
+     * @param width  需要添加水印的图片宽度
      * @param height 需要添加水印的图片长度
      * @return
      */
-    private static BufferedImage handleTextWaterMark(String text, int width, int height, int fontSize) {
+    private static BufferedImage handleTextWaterMark(String text, int width, int height) {
+        int size = width > height ? height : width;
+        //最小字体大小
+        int minFontSize = 5;
+        //文字缩小比例，根据图片大小进行动态设置字体大小
+        int reduceScale = 30;
+        //根据图片大小动态设置字体大小
+        int fontSize = size / reduceScale < minFontSize ? minFontSize : size / reduceScale;
 
-        final Font font = new Font("宋体", Font.BOLD, fontSize);
+        final Font font = new Font("微软雅黑", Font.BOLD, fontSize);
 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
@@ -88,11 +155,12 @@ public class ImageOperationUtils {
 
         g = image.createGraphics();
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setColor(Color.BLACK);
+        g.setColor(Color.white);
         g.setFont(font);
 
         if (StringUtils.isNotBlank(text)) {
-            g.drawString(text, 5, height - fontSize / 2);
+            g.drawString(text, fontSize, height - fontSize * 2);
+            g.drawString(AthenaDateUtils.format(new Date(), AthenaDateUtils.FULL_DATE_TIME_FORMAT.getPattern()), fontSize, height - fontSize / 2);
         }
 
         g.dispose();
--- src/test/java/com/foutin/date/ImageUtilsTest.java
+++ src/test/java/com/foutin/date/ImageUtilsTest.java
@@ -4,6 +4,11 @@ import com.foutin.utils.AthenaDateUtils;
 import com.foutin.utils.BaseTest;
 import com.foutin.utils.ImageOperationUtils;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 /**
  * @author xingkai.fan
  * @description
@@ -12,11 +17,19 @@ import com.foutin.utils.ImageOperationUtils;
 public class ImageUtilsTest extends BaseTest {
 
     public static void main(String[] args) throws Exception {
-        String resource = "F:/4619.jpg";
-        String target = "F:/image-with-watermark.jpg";
+        String resource = "F:/IMG_20190406_180608.jpg";
+//        String resource = "F:/small_picture.jpg";
+        String target = "F:/out-picture/image-with-watermark.jpg";
         String warter = "F:/water9.png";
 //        ImageOperationUtils.setImageWatermark(resource, target, warter);
-        String text = "车架号xxxx" + AthenaDateUtils.getDate();
-        ImageOperationUtils.setTextWatermark(resource, target, text, 20);
+        String text = "车架号xxxx";
+//        ImageOperationUtils.setTextWatermark(resource, target, text);
+        OutputStream outputStream = new FileOutputStream("F:/out-picture/image-with-watermark.png");
+        ImageOperationUtils.setTextWatermark(resource, outputStream, text);
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//        BufferedImage bufferedImage = ImageOperationUtils.setTextWatermark(resource, text);
+//        ImageIO.write(bufferedImage, "jpg", os);
+//        InputStream is = new ByteArrayInputStream(os.toByteArray());
+
     }
 }
